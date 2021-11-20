f0e57e3861c78493769b63ab8b8475c8e868fc84
Ahmed Elatrouz <aelatrouz@smb.sqli.com>
2021-11-20 17:27:36 +0100

    initial commit

---
+++ src/main/java/me/ahmed/repos/ReposApplication.java
@@ -0,0 +1,13 @@
+package me.ahmed.repos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ahmed/repos/ReposApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ahmed.repos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
